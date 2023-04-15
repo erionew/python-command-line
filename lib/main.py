98f6dba8d2e1a762a41d7f293fff6143e8ecec1c
Erione White <erionewhite1999@gmail.com>
2023-04-15 12:51:17 -0500

    returns a formated list of contacts if there are any

--- lib/main.py
+++ lib/main.py
@@ -2,9 +2,19 @@ from connection import Contact
 
 print('Contact Book')
 
-person = Contact(first_name='person',last_name='one',phone_number='214-212-222')
-person.save()
 
 contact_list = Contact.select()
 
-print([contact.first_name for contact in contact_list])
+person = Contact(first_name='person',last_name='one',phone_number='214-212-222')
+person.save()
+
+person = Contact(first_name='person',last_name='two',phone_number='214-212-223')
+person.save()
+
+if len(contact_list) == 0:
+     print('Your contacts are empty.')
+else:
+     contact_info = [contact.first_name.capitalize() + ' ' + contact.last_name.capitalize() + ' - ' + contact.phone_number for contact in contact_list]
+     for contact in contact_info:
+        print(f'{contact_info.index(contact)+1}.{contact}')
+
