b38ae46cfa42acb0df7b8f8f006e14cdac4553b4
Erione White <erionewhite1999@gmail.com>
2023-04-15 13:00:59 -0500

    created function that prints contact book. asks for user input to begin creating contact info

--- lib/main.py
+++ lib/main.py
@@ -1,20 +1,34 @@
 from connection import Contact
 
-print('Contact Book')
 
 
 contact_list = Contact.select()
 
-person = Contact(first_name='person',last_name='one',phone_number='214-212-222')
-person.save()
+# person = Contact(first_name='person',last_name='one',phone_number='214-212-222')
+# person.save()
 
-person = Contact(first_name='person',last_name='two',phone_number='214-212-223')
-person.save()
+# person = Contact(first_name='person',last_name='two',phone_number='214-212-223')
+# person.save()
 
-if len(contact_list) == 0:
-     print('Your contacts are empty.')
+def print_contact_book():
+
+    print('Contact Book')
+    if len(contact_list) == 0:
+        print('Your contacts are empty.')
+    else:
+        contact_info = [contact.first_name.capitalize() + ' ' + contact.last_name.capitalize() + ' - ' + contact.phone_number for contact in contact_list]
+        for contact in contact_info:
+            print(f'{contact_info.index(contact)+1}.{contact}')
+
+print_contact_book()
+
+
+user_input = input('Would you like to add a new contact? (y/n): ')
+
+if user_input == 'y':
+    contact_first_name = input('Contact First Name: ')
+    contact_last_name = input('Contact Last Name: ')
+    contact_phone = input('Contact Phone Number: ')
+    print('Contact Added')
 else:
-     contact_info = [contact.first_name.capitalize() + ' ' + contact.last_name.capitalize() + ' - ' + contact.phone_number for contact in contact_list]
-     for contact in contact_info:
-        print(f'{contact_info.index(contact)+1}.{contact}')
-
+    print(':/')
