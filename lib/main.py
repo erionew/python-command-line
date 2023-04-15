1e8627d1bbad1272ba91a9b367ca52df2c52aedb
Erione White <erionewhite1999@gmail.com>
2023-04-15 11:58:52 -0500

    created contact model

--- lib/main.py
+++ lib/main.py
@@ -14,3 +14,8 @@ db.connect()
 class BaseModel(Model):
     class Meta:
         database = db
+
+class Contact(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    phone_number = CharField()
